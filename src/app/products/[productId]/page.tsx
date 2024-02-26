c55c06175591fa44396110878d7657b765598046
preyaswi <preyaswiprakashan@gmail.com>
2024-02-26 17:42:27 +0530

    add a name to the metadata

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,7 +1,20 @@
-export default function ProductDetails({
-    params,
-}:{
-    params:{productId:string};
-}){
+import { Metadata } from "next";
+type props={
+    params:{
+        productId:string;
+    };
+};
+export const generateMetadata =async ({params}:props):Promise<Metadata>=>{
+    const title=await new Promise(resolve=>{
+        setTimeout(()=>{
+            resolve(`iphone ${params.productId}`);
+        },100);
+    });
+    return{
+        title:`product ${title}`
+    }
+}
+
+export default function ProductDetails({ params}:props){
     return <h1>product detail page {params.productId}</h1>;
 }
